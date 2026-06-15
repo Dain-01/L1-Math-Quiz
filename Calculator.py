9fa553aaf85f47f3dd4a7301cf2cf61aad669af3
Dain-01 <ioaneo60807@masseyhigh.school.nz>
2026-06-15 15:07:32 +1200

    Not finished, need to add random sign list

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+import random
+
+# Initializes total to 0
+total = 0
+
+# Takes two random numbers
+one = random.randint(1, 10)
+two = random.randint(1, 10 )
+
+
+
+# Adds two random numbers
+total += one + two
+
+#Output the results
+
+print(f" {one} + {two} = {total} ")
